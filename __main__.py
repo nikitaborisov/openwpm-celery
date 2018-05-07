c728cc2f8a603c3d0bf1c788d50db313147c05eb
Nikita Borisov <git@nikita.ca>
2018-05-07 14:56:20 +0000

    add BATCH constant, set to 500

--- __main__.py
+++ __main__.py
@@ -4,14 +4,15 @@ from .tasks import run_crawl
 import os.path
 
 scan_name = sys.argv[1]
-NUM_SITES = 100000
+NUM_SITES = 1000000
+BATCH = 500
 
 
-for i in range(1, NUM_SITES, 1000):
+for i in range(1, NUM_SITES, BATCH):
     if len(sys.argv) > 2 and sys.argv[2] == "missing":
-        if os.path.exists("/efs/crawls/{}/{}-{}".format(scan_name, i, i+999)):
+        if os.path.exists("/efs/crawls/{}/{}-{}".format(scan_name, i, i+BATCH-1)):
             continue
-    run_crawl.delay(scan_name, i, i+999)
+    run_crawl.delay(scan_name, i, i+BATCH-1)
 
 
 
